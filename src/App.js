d0d28d7844ae3d969ffee6cff27bb9025b54fadd
Seun Oshitade <seun.oshitade@gmail.com>
2022-07-22 14:15:11 -0600

    add reactrouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    
+    <h1>This is the beginning of Jabber!</h1>
+    <Router>
+      <>
+        <Switch>
+          <Route path="/" exact>
+            <h1> This is the homepage </h1>
+          </Route>
+        </Switch>
+      </>
+    </Router>
+
     </div>
   );
 }
