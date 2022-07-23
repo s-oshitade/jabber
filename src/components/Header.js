7f33758ca68b2b51f31d09eda9ecae2a71500fe2
Dino <dinopranjic92@gmail.com>
2022-07-23 15:55:37 -0400

    add Header Left component and icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,13 +22,34 @@ function Header () {
 export default Header;
 
 const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(--main-color1);
+  color: white;
 
 `;
 
 const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
 
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+
+  }
 `;
 
 const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
 
+  :hover {
+    opacity: 0.8;
+  }
 `;
