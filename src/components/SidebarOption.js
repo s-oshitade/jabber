dd4676b57ea67ed116de9ff4066cf8df46c7108d
Seun Oshitade <seun.oshitade@gmail.com>
2022-07-23 16:56:38 -0600

    Add onClick to SidebarOptionContainer

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -2,8 +2,17 @@ import React from 'react';
 import styled from "styled-components";
 
 function SidebarOption ({ Icon, title, addChannelOption }) {
+
+  const addChannel = () => {
+    
+  };
+
+  const selectChannel = () => {};
+
   return (
-    <SidebarOptionContainer>
+    <SidebarOptionContainer
+      onClick={addChannelOption ? addChannel : selectChannel}
+    >   
       {Icon && <Icon fontSize='small' style={{ padding: 10 }}/>}
       {Icon ? (
         <h3>{title}</h3>
