35b9eeaf924292749744f0305a685c1b82e0bb3e
Dino <dinopranjic92@gmail.com>
2022-07-23 18:06:07 -0400

    Style SidebarOption component

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -22,6 +22,23 @@ export default SidebarOption;
 
 const SidebarOptionContainer = styled.div`
   display: flex;
+  font-size: 12px;
+  align-items: center;
+  padding-left: 2px;
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.9;
+    background-color: #154c79;
+  }
+
+  > h3 {
+    font-weight: 500;
+  }
+
+  > h3 > span {
+    padding: 15px;
+  }
 `;
 
 const SidebarOptionChannel = styled.div`
