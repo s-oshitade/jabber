30516f263b791d3619e923e0eb8bf2fd0c2934af
Dino <dinopranjic92@gmail.com>
2022-07-23 16:59:43 -0400

    Add Sidebar Header

---
+++ src/components/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from "styled-components";
+
+function Sidebar() {
+  return (
+    <SidebarContainer>
+      <SidebarHeader>
+
+      </SidebarHeader>
+
+    </SidebarContainer>
+  )
+}
+
+export default Sidebar;
+
+const SidebarContainer = styled.div`
+
+`;
+
+const SidebarHeader = styled.div`
+
+`;
