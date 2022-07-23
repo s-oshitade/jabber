9505e1ea07d928fca613416d0eaa854aa516ec7b
Dino <dinopranjic92@gmail.com>
2022-07-23 17:05:56 -0400

    Add Sidebar Header info and icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import styled from "styled-components";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord"
 
 function Sidebar() {
   return (
     <SidebarContainer>
       <SidebarHeader>
+        <SidebarInfo>
+          <h2>Welcome to Jabber</h2>
+          <h3>
+            <FiberManualRecordIcon />
+            Jabber User
+          </h3>
+
+        </SidebarInfo>
 
       </SidebarHeader>
 
@@ -19,5 +28,9 @@ const SidebarContainer = styled.div`
 `;
 
 const SidebarHeader = styled.div`
+
+`;
+
+const SidebarInfo = styled.div`
 
 `;
