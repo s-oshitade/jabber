d1c7e8c5229f9a8079bf10c4010255226c03cf93
mbbaltaji <baltaji@uwindsor.ca>
2022-07-24 15:44:21 -0400

    Add styling to ChatInput component (form, button and textfield)

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -22,4 +22,23 @@ export default ChatInput
 const ChatInputContainer = styled.div`
   border-radius: 20px;
 
+  > form {
+    position: relative;
+    display: flex;
+    justify-content: center;
+  }
+
+  > form > input {
+    position: fixed;
+    bottom: 30px;
+    width: 60%;
+    border: 1px solid gray;
+    border-radius: 3px;
+    padding: 20px;
+    outline: none;
+  }
+
+  > form > button {
+    display: none !important;
+  }
 `;
