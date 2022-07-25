66d1208abf60534842046af0ace8569b9c7e5bc0
Seun Oshitade <seun.oshitade@gmail.com>
2022-07-25 14:48:38 -0600

    Incorporate user details dynamically

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import styled from "styled-components";
 import { Avatar } from "@material-ui/core";
-import AccessTimeIcon from "@material-ui/icons/AccessTime"
-import SearchIcon from "@material-ui/icons/Search"
-import HelpOutlineIcon from "@material-ui/icons/HelpOutline"
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
 
 function Header () {
+  const [user] = useAuthState(auth);
+
   return (
     <HeaderContainer>
       <HeaderLeft>
         <HeaderAvatar
-        // TODO: ADD ONCLICK 
+        onClick={() => auth.signOut()}
+         src={user?.photoURL}
+         alt={user?.displayName}
         />
         <AccessTimeIcon />
       </HeaderLeft>
