f4741d124f9da8b91e67c6b38596d9f63b47ab93
Seun Oshitade <seun.oshitade@gmail.com>
2022-07-27 14:21:15 -0600

    Uncomment sidebar options

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,14 +3,14 @@ import styled from "styled-components";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
 import SidebarOption from './SidebarOption';
-// import InsertCommentIcon from "@material-ui/icons/InsertComment";
-// import InboxIcon from "@material-ui/icons/Inbox";
-// import DraftsIcon from "@material-ui/icons/Drafts";
-// import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
-// import FileCopyIcon from "@material-ui/icons/FileCopy";
-// import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
-// import AppsIcon from "@material-ui/icons/Apps";
-// import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add";
 import { auth, db } from "../firebase";
@@ -37,14 +37,14 @@ function Sidebar() {
           <CreateIcon />
       </SidebarHeader>
 
-      {/* <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+      <SidebarOption Icon={InsertCommentIcon} title="Threads" />
       <SidebarOption Icon={InboxIcon} title="Mentions & Reactions" />
       <SidebarOption Icon={DraftsIcon} title="Saved Items" />
       <SidebarOption Icon={BookmarkBorderIcon} title="Channel Browser" />
       <SidebarOption Icon={PeopleAltIcon} title="People & User Groups" />
       <SidebarOption Icon={AppsIcon} title="Apps" />
       <SidebarOption Icon={FileCopyIcon} title="File Browser" />
-      <SidebarOption Icon={ExpandLessIcon} title="Show Less" /> */}
+      <SidebarOption Icon={ExpandLessIcon} title="Show Less" />
       <hr />
       <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
       <hr />
