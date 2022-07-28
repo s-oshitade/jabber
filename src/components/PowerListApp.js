0f39f0aefe127977df93d7f351e0b1536bb569f1
Seun Oshitade <seun.oshitade@gmail.com>
2022-07-28 10:37:36 -0600

    Create PowerListApp component

---
+++ src/components/PowerListApp.js
@@ -0,0 +1,15 @@
+import PowerItem from "./PowerItem";
+import PowerInput from "./PowerInput";
+
+import React from 'react'
+
+function PowerListApp() {
+  return (
+    <div className="PowerListApp">
+      <PowerInput />
+      <PowerItem />
+    </div>
+  )
+}
+
+export default PowerListApp
