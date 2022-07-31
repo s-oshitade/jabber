8b05543ad2fd290f65b1f48580225bd8d1f3beb8
Seun Oshitade <seun.oshitade@gmail.com>
2022-07-31 05:55:39 -0600

    Update database wrt state of task completion

--- src/components/PowerItem.js
+++ src/components/PowerItem.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { auth, db } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 import DeleteIcon from "@material-ui/icons/Delete";
 
+
 function PowerItem({id, task, done}) {
+  const [user] = useAuthState(auth);
+  const userEmail = user?.email;
 
   function handleFinish(e) {
-    //...
+   console.log("I got clicked!")
+   console.log(id)
+    //Update the database wrt the state of done
+    if (done === false){
+      db.collection("users").doc("todoLists").collection(userEmail).doc(id).update({
+        done: true
+      });
+    } else {
+      db.collection("users").doc("todoLists").collection(userEmail).doc(id).update({
+        done: false
+      });
+    }
   }
 
   function handleRemove(e) {
