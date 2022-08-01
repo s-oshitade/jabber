03334f87c78b9a18fa9d18a6fa58e906da0511c2
mbbaltaji <mbbaltaji@gmail.com>
2022-08-01 15:31:32 -0400

    Make fetch request to spotify API to get users sop played songs

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,29 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import SpotifyPlayer from 'react-spotify-web-playback';
-
+import styled from "styled-components";
 
 function MusicPlayer({token}) {
 
+  const [playBackUris, setPlayBackUris] = useState([]);
+  useEffect ( ()=> {
+    console.log('token:', token);
+    if (token) {
+      fetch('https://api.spotify.com/v1/me/top/tracks', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        method: 'GET'
+      })
+      .then((res) => {
+        return res.json()
+      })
+      .then((topItems) => {
+        setPlayBackUris(topItems.items.map((item) => item.uri))
+      })
+    }
+    
+  },[token]) 
+
 
 return (
-  <SpotifyPlayer
-  uris={["spotify:artist:3TVXtAsR1Inumwj472S9r4"]}
-  token={token}
-  persistDeviceSelection={true}
-  showSaveIcon={true}
-  syncExternalDevice={true}
-  styles={{
-    activeColor: '#fff',
-    bgColor: 'none',
-    color: '#fff',
-    loaderColor: '#fff',
-    sliderColor: '#1cb954',
-    trackArtistColor: '#ccc',
-    trackNameColor: '#fff',
-  }}
-/>
+  
+  <SpotifyPlayerContainer >
+    <SpotifyPlayer
+      uris={playBackUris}
+      token={token}
+      persistDeviceSelection={true}
+      showSaveIcon={true}
+      syncExternalDevice={true}
+      styles={{
+        activeColor: '#fff',
+        bgColor: 'none',
+        color: '#fff',
+        loaderColor: '#fff',
+        sliderColor: '#1cb954',
+        trackArtistColor: '#ccc',
+        trackNameColor: '#fff',
+      }}  
+    />
+  </SpotifyPlayerContainer>
 );
-    
-  }
+}
+
+const SpotifyPlayerContainer = styled.div`
+margin-left: 1em;
+margin-right: 1em;
+`
+
 
 export default MusicPlayer
