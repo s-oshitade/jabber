0a1592b42e93e38e9db224021d2a24e14005ea71
Seun Oshitade <seun.oshitade@gmail.com>
2022-08-01 17:35:53 -0600

    Show music player

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,59 +1,59 @@
-// import React, { useState, useEffect } from 'react';
-// import SpotifyPlayer from 'react-spotify-web-playback';
-// import styled from "styled-components";
+import React, { useState, useEffect } from 'react';
+import SpotifyPlayer from 'react-spotify-web-playback';
+import styled from "styled-components";
 
-// function MusicPlayer({token}) {
+function MusicPlayer({token}) {
 
-//   const [playBackUris, setPlayBackUris] = useState([]);
-//   useEffect ( ()=> {
-//     console.log('token:', token);
-//     if (token) {
-//       fetch('https://api.spotify.com/v1/me/top/tracks', {
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//           'Content-Type': 'application/json'
-//         },
-//         method: 'GET'
-//       })
-//       .then((res) => {
-//         return res.json()
-//       })
-//       .then((topItems) => {
-//         setPlayBackUris(topItems.items.map((item) => item.uri))
-//       })
-//     }
+  const [playBackUris, setPlayBackUris] = useState([]);
+  useEffect ( ()=> {
+    console.log('token:', token);
+    if (token) {
+      fetch('https://api.spotify.com/v1/me/top/tracks', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        method: 'GET'
+      })
+      .then((res) => {
+        return res.json()
+      })
+      .then((topItems) => {
+        setPlayBackUris(topItems.items.map((item) => item.uri))
+      })
+    }
     
-//   },[token]) 
+  },[token]) 
 
 
-// return (
+return (
   
-//   <SpotifyPlayerContainer >
-//     <SpotifyPlayer
-//       uris={playBackUris}
-//       token={token}
-//       persistDeviceSelection={true}
-//       showSaveIcon={true}
-//       syncExternalDevice={true}
-//       styles={{
-//         activeColor: '#fff',
-//         bgColor: 'none',
-//         height: '2em',
-//         color: '#fff',
-//         loaderColor: '#fff',
-//         sliderColor: '#1cb954',
-//         trackArtistColor: '#ccc',
-//         trackNameColor: '#fff',
-//       }}  
-//     />
-//   </SpotifyPlayerContainer>
-// );
-// }
+  <SpotifyPlayerContainer >
+    <SpotifyPlayer
+      uris={playBackUris}
+      token={token}
+      persistDeviceSelection={true}
+      showSaveIcon={true}
+      syncExternalDevice={true}
+      styles={{
+        activeColor: '#fff',
+        bgColor: 'none',
+        height: '2em',
+        color: '#fff',
+        loaderColor: '#fff',
+        sliderColor: '#1cb954',
+        trackArtistColor: '#ccc',
+        trackNameColor: '#fff',
+      }}  
+    />
+  </SpotifyPlayerContainer>
+);
+}
 
-// const SpotifyPlayerContainer = styled.div`
-// margin-left: 1em;
-// margin-right: 1em;
-// `
+const SpotifyPlayerContainer = styled.div`
+margin-left: 1em;
+margin-right: 1em;
+`
 
 
-// export default MusicPlayer
+export default MusicPlayer
