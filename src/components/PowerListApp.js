06f3b163b70edaf73d83e02c2f7fdd22353b59fc
Seun Oshitade <seun.oshitade@gmail.com>
2022-08-06 11:59:31 -0600

    Pass edit state down to NewTaskForm component

--- src/components/PowerListApp.js
+++ src/components/PowerListApp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { auth, db } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
@@ -10,18 +10,30 @@ function PowerListApp() {
   const [user] = useAuthState(auth);
   const userEmail = user?.email
   const[todos] = useCollection(db.collection("users").doc("todoLists").collection(userEmail))
+  const[editText, setEditText] = useState("");
+  const[id, setId] = useState("");
+
+  function editItem (text, id) {
+    console.log("Received edit ping")
+    console.log(text)
+    setEditText(text)
+    setId(id)
+  }
 
   return (
     <div className="PowerListApp">
       <span>{user.displayName}'s List</span>
       <hr />
-      <NewTaskForm />
+      <NewTaskForm editInput={editText} id={id}/>
       <ul>
         {todos?.docs.map((doc) => (
           <PowerItem key={doc.id} 
             id={doc.id}
             task={doc.data().task}
-            done={doc.data().done}></PowerItem>
+            done={doc.data().done}
+            edit={editItem}
+            >
+          </PowerItem>
           ))}
       </ul>
     </div>
