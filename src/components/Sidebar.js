bd584cba8162810057f82de92e1ddaf3a41ace14
Seun Oshitade <seun.oshitade@gmail.com>
2022-08-09 13:00:36 -0600

    Import DashBoardIcon for channels

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,15 +4,8 @@ import styled from "styled-components";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
 import SidebarOption from './SidebarOption';
-import InsertCommentIcon from "@material-ui/icons/InsertComment";
-import InboxIcon from "@material-ui/icons/Inbox";
-import DraftsIcon from "@material-ui/icons/Drafts";
-import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
-import FileCopyIcon from "@material-ui/icons/FileCopy";
-import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
-import AppsIcon from "@material-ui/icons/Apps";
-import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import DashboardIcon from "@material-ui/icons/Dashboard"
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 import AddIcon from "@material-ui/icons/Add";
 import { auth, db } from "../firebase";
@@ -52,7 +45,7 @@ function Sidebar({token}) {
        <SidebarOption Icon={LibraryMusicIcon} title="Music Player" openSpotifyLogin={openSpotifyLogin('/auth/login')}/>
        
       <hr />
-      <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+      <SidebarOption Icon={DashboardIcon} title="Channels" />
       <hr />
       <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel" />
       {channels?.docs.map((doc) => (
