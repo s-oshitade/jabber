6e3cb3230e6083af48029baa66ee23bdb245d5e9
mbbaltaji <mbbaltaji@gmail.com>
2022-08-10 18:33:14 -0400

    Import Dialog from MUI

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -18,6 +18,12 @@ import NoteAddIcon from '@material-ui/icons/NoteAdd';
 import  TextField  from '@material-ui/core/TextField/TextField';
 import { ClickAwayListener } from '@material-ui/core';
 
+import Dialog from '@material-ui/core/Dialog/Dialog';
+import DialogActions from '@material-ui/core/DialogActions/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle/DialogTitle';
+import Button from '@material-ui/core/Button/Button';
+
 
 function Chat() {
   const [user] = useAuthState(auth);
